3b80a56a87aa9a10e47c0826454d0e9fb54d33e3
kruore <kruore1321@gmail.com>
2021-11-24 17:51:56 +0900

    1124
    1124 Server

---
+++ KINL_Server/KINL_Server/KINL_Server/ClientData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KINL_Server
+{
+    class ClientData
+    {
+        public TcpClient client { get; set; }
+        public byte[] readByteData { get; set; }
+
+        public ClientData(TcpClient client)
+        {
+            this.client = client;
+            this.readByteData = new byte[1024];
+        }
+
+    }
+}
---
+++ KINL_Server/KINL_Server/KINL_Server/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+public enum DataType
+{
+    Text = 0, File = 1, Image = 2, Video = 3
+}
+
+class Server
+{
+    public Server()
+    {
+        AsyncServerStart();
+    }
+    
+    private void AsyncServerStart()
+    {
+        int port = 4545;
+        TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+        listener.Start(); 
+        Console.WriteLine("서버를 시작합니다. 접속 대기중");
+
+        TcpClient acceptClient = listener.AcceptTcpClient();
+        Console.WriteLine("클라이언트 접속성공.");
+
+        ClientData clientData = new ClientData(acceptClient);
+
+
+        while (true)
+        {
+            TcpClient acceptClient = listener.AcceptTcpClient();
+            ClientData 
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+        string host = Dns.GetHostName();
+
+        // Port Define 4545
+        int portNum = 4545;
+
+
+        IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, portNum);
+        Console.WriteLine($" Server Open : Host IP = {iPAddress} ");
+
+        while (true)
+        {
+            ;
+        }
+    }
+
+}
