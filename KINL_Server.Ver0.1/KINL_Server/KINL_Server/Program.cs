d065badf62a098622147b9edd6d31d94a545a529
kruore <33648811+kruore@users.noreply.github.com>
2021-12-09 14:46:51 +0900

    1209
    1209_ Server_ Unity

---
+++ KINL_Server.Ver0.1/KINL_Server/KINL_Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+class Parogram
+{
+    static void Main(string[] args)
+    {
+        string host= Dns.GetHostName();
+        int port = 4545;
+        IPAddress iPAddress = IPAddress.Any;
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        socket.Connect(iPEndPoint);
+        Console.WriteLine($"Connect Server");
+
+        byte[] buffer = new byte[1024];
+        int sendBytes = socket.Send(buffer);
+
+        byte[] recvBuff = new byte[1024];
+        int recvBytes = socket.Receive(recvBuff);
+        string recvData = Encoding.UTF8.GetString()
+
+    }
+}
