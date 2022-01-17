bcff093ccf294857d910b233716e42a14ccf15aa
kruore <33648811+kruore@users.noreply.github.com>
2022-01-17 19:26:40 +0900

    Revert "Merge branch 'main' of https://github.com/kruore/Server"
    This reverts commit 143655ce7d5ee5f7fb969d69f92144da3e86d143, reversing
    changes made to 8ca76fa0941c584a7f5ddb35c3549d17d86189f7.

--- KINL_WPF_Server/KINL_Server_WPF/KINL_Server_WPF/ClientData.cs
+++ KINL_WPF_Server/KINL_Server_WPF/KINL_Server_WPF/ClientData.cs
@@ -25,8 +25,6 @@ namespace KINL_Server_WPF
 
         public ClientData(TcpClient client)
         {
-
-            //Client Data Save
             this._client = client;
             this._recvData = new byte[1024];
             this._sendData = new byte[1024];
